63140b9a23d4b5cf7364281bcc5b0ca50c4cb904
AsherHoltham <asher.holtham.github@gmail.com>
2025-03-01 16:10:21 -0800

    APPLICATION UI: ENCAPSULATED STOOP PROGRAM INTO WIDGET THAT CAN BE CALLED TO BY MAIN PAGE, ON START ALL TIME SIGNATURES REMAIN CONSTANT 1503millisec POST PRERSENTATION OF STIMULI

--- stroop/test/widget_test.dart
+++ stroop/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:stroop/main.dart';
+import 'package:stroop/stroopApp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
