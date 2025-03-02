a4c9ac29e01ea09c2da5dcd3d212777138ae74c8
AsherHoltham <asher.holtham.github@gmail.com>
2025-03-02 14:57:56 -0800

    TEST CODE: GENERATED INITIAL FILE -> WORKFLOW PASS ONE

--- stroop/test/widget_test.dart
+++ stroop/test/widget_test.dart
@@ -5,26 +5,16 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:stroop/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('MainApplication smoke test', (WidgetTester tester) async {
+    // Build the MainApplication widget.
     await tester.pumpWidget(const MainApplication());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the "Stroop Task" text exists in the widget tree.
+    expect(find.text('Stroop Task'), findsOneWidget);
   });
 }
